daa3a02ac5371bb46eae153841392dfedac23d7a
chitboon <chitboon@gmail.com>
2015-03-11 20:22:24 +0800

    Initial commit.

---
+++ Quest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quest
+//
+//  Created by Chit Boon Lee on 3/11/15.
+//  Copyright (c) 2015 Nanyang Polytechnic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
